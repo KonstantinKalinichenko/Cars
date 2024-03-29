62eb73e5da5073262968ef1dcbe72a1075931a8d
KonstantinKalinichenko <kostyak90@gmail.com>
2024-03-29 22:11:54 +0300

    change

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import calculator
 if __name__ == '__main__':
     calc = calculator.Calculator(years = 10)
     calc.add_car(
-        calculator.Car("Toyota Corolla", 30000, 7, 1200, 2500),
+        calculator.Car("Toyota Corolla", 40000, 7, 1200, 2500),
     )
     calc.add_car(
-        calculator.ElectricCar("Tesla Model 3", 200000, 5500, 150),
+        calculator.ElectricCar("Tesla Model 3", 200001, 5500, 150),
     )
 
     calc.print_cars()
